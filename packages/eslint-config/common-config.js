e0013f51771b6c2609f70945674b98c39d2b568e
visrut <visrutnavadiya111@gmail.com>
2024-04-07 13:57:24 +0530

    Inital commit

---
+++ packages/eslint-config/common-config.js
@@ -0,0 +1,29 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "eslint-config-turbo",
+  ],
+  env: {
+    node: true,
+    browser: true,
+  },
+  plugins: ["only-warn"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+  ]
+};
